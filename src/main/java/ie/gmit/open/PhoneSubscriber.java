9f33be8a80c4b4fd0eb6738188df784156988cb2
catha74 <cathaljwalsh@hotmail.com>
2020-03-26 21:19:05 +0000

    Finished

--- src/main/java/ie/gmit/open/PhoneSubscriber.java
+++ src/main/java/ie/gmit/open/PhoneSubscriber.java
@@ -2,15 +2,10 @@ package ie.gmit.open;
 
 import java.util.List;
 
-public class PhoneSubscriber {
-
-    private Long subscriberId;
-
-    private String address;
-
-    private Long phoneNumber;
+public class PhoneSubscriber extends Subscriber {
 
     private int baseRate;
+
 
     public double calculateBill() {
         List<CallHistory.Call> sessions = CallHistory.getCurrentCalls(subscriberId);
@@ -18,54 +13,7 @@ public class PhoneSubscriber {
         return totalDuration*baseRate/100;
     }
 
-    /**
-     * @return the subscriberId
-     */
-    public Long getSubscriberId() {
-        return subscriberId;
-    }
 
-    /**
-     * @param subscriberId the subscriberId to set
-     */
-    public void setSubscriberId(Long subscriberId) {
-        this.subscriberId = subscriberId;
-    }
-
-    /**
-     * @return the address
-     */
-    public String getAddress() {
-        return address;
-    }
-
-    /**
-     * @param address the address to set
-     */
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    /**
-     * @return the phoneNumber
-     */
-    public Long getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    /**
-     * @param phoneNumber the phoneNumber to set
-     */
-    public void setPhoneNumber(Long phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    /**
-     * @return the baseRate
-     */
-    public int getBaseRate() {
-        return baseRate;
-    }
 
     /**
      * @param baseRate the baseRate to set
--- src/main/java/ie/gmit/single/UserController.java
+++ src/main/java/ie/gmit/single/UserController.java
@@ -10,61 +10,28 @@ public class UserController {
 
     //Handles incoming JSON requests that work on User resource/entity
 
-        //Store used by controller
-        private Store store = new Store();
+    //Store used by controller
+    private UserPersistenceService persistenceService = new UserPersistenceService();
 
-        //Create a new user
-        public String createUser(String userJson) throws IOException {
-            ObjectMapper mapper = new ObjectMapper();
+    //Create a new user
+    public String createUser(String userJson) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
 
-            User user = mapper.readValue(userJson, User.class);
+        User user = mapper.readValue(userJson, User.class);
 
-            if(!isValidUser(user)) {
-                return "ERROR";
-            }
+        ValidateUser myUser = new ValidateUser();
 
-            store.store(user);
+        boolean checkedUser = myUser.checkUser(user);
 
-            return "SUCCESS";
+        if (!checkedUser) {
+            return "ERROR";
         }
 
-        //Validates the user object
-        private boolean isValidUser(User user) {
-            if(!isPresent(user.getName())) {
-                return false;
-            }
-            user.setName(user.getName().trim());
+        persistenceService.saveUser(user);
 
-            if(!isValidAlphaNumeric(user.getName())) {
-                return false;
-            }
-            if(user.getEmail() == null || user.getEmail().trim().length() == 0) {
-                return false;
-            }
-            user.setEmail(user.getEmail().trim());
-            if(!isValidEmail(user.getEmail())) {
-                return false;
-            }
-            return true;
-        }
 
-        //Simply checks if value is null or empty..
-        private boolean isPresent(String value) {
-            return value != null && value.trim().length() > 0;
-        }
-        //check string for special characters
-        private boolean isValidAlphaNumeric(String value) {
-            Pattern pattern = Pattern.compile("[^A-Za-z0-9]");
-            Matcher matcher = pattern.matcher(value);
-            return !matcher.find();
-        }
-        //check string for valid email address - this is not for prod.
-        //Just for demo. This fails for lots of valid emails.
-        private boolean isValidEmail(String value) {
-            Pattern pattern = Pattern.compile("^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$");
-            Matcher matcher = pattern.matcher(value);
-            return matcher.find();
-        }
+        return "SUCCESS";
+    }
 
- }
+}
 
